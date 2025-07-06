93e9816d219f732fa511ce90e72c5ba70d07a6f2
ArslanMDOfficialByTesting <muhammadarslanmdofficial@gmail.com>
2025-07-06 23:43:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ AUTO_REACT: process.env.AUTO_REACT || "true", // Enable/disable auto reaction (t
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"false",
 OMDB_API_KEY: "5e339fb7",
