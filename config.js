b542ad96b55f726e07db93adaf8c30ebf4c2a7d8
ArslanMDOfficialByTesting <muhammadarslanmdofficial@gmail.com>
2025-07-08 00:07:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ARSL~h3kEnBjZ#FrY4fm56YGkMFl-oMjvx42yQ-P7PZLwtK478pvtnNBI",  // ADD YOUR SESSION ID 
+SESSION_ID: process.env.SESSION_ID || "ARSL~E60DlTSB#BeYiO5ERg3F5obC7tdnGDMIp6Kv0r5VlYAEWfO7go2I",  // ADD YOUR SESSION ID 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/atby2t.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that Arslan-Ultra-MD is ALIVE now—ready to dive into new adventures with you!",
 SUDO_NB: process.env.SUDO_NB || "923237045919",
-AUTO_REACT: process.env.AUTO_REACT || "true", // Enable/disable auto reaction (true/false)
+AUTO_REACT: process.env.AUTO_REACT || "false", // Enable/disable auto reaction (true/false)
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
